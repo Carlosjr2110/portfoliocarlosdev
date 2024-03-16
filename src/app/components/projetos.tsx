7a5e3069123e747065a333e20ea38ef54febd69e
Carlos H Chaves <carloshumberto2110@gmail.com>
2024-03-15 22:23:48 -0300

    second commit

--- src/app/components/projetos.tsx
+++ src/app/components/projetos.tsx
@@ -11,7 +11,7 @@ export const Projetos = ({label, onClick}: Props) => {
     
     return (
         <div className="flex items-center mb-8">
-                <div onClick={onClick} className="w-2 h-4 cursor-pointer">
+                <div onClick={onClick} onTouchStart={onClick} className="w-2 h-4 cursor-pointer">
                 <Cube  logo={logo.src} width="32px" height="32px" transform="16px"/>
                 </div>
                 <div  className="text-white w-24 ml-12 text-[12px] md:w-36 md:text-[17px] ">{label}</div>   
